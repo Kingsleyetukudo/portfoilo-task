a7a2a350bd4c862ddc2a37887f1b49807658325f
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-01-24 14:39:55 +0100

    nuxt installed

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container border">
+    <nav>
+      <ul class="flex justify-end">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+
+    <div class="container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>About The Portfolio</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
